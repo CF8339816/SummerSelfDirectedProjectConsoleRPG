a14250ff90b8023269569d1217c95336f4d726cb
CF8339816 <chris_french_3dga@hotmail.com>
2026-08-21 15:36:12 -0300

    working on PC restructure
    working on whether i need to remove base character class or if i can incorporate it in at the pc class level without breaking the archtype class connection.

--- SummerSelfDirectedProjectConsoleRPG/PC.cs
+++ SummerSelfDirectedProjectConsoleRPG/PC.cs
@@ -1,57 +1,42 @@
+using SummerSelfDirectedProjectConsoleRPGusing;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SummerSelfDirectedProjectConsoleRPG
 
+{ 
 
-	public class PC 
-{
+    public class PC : Character
+    {
 
-	private const int PcHp = 100;
-    public string Name { get; }
+        private const int PcHp = 100;
+        public string Name { get; }
 
-    public static int plXP = 0;
-	public static int plLevel = 0;
+        public static int plXP = 0;
+        public static int plLevel = 0;
 
-	public static string arcType = ArcType.arcType;
+        public int MaxHP { get; }
 
-	public static string nameUsed;
+        public int ArmorClass { get; }
+        public ArchType Profession { get; }
 
-	public PC(string type, string name, int hp, int speed, int dex, int ac, string magic, int strength, char symbol, ConsoleColor color, int attack,
-	 int mana, string nameUsed, string arcType, int plHpPerLevBonus, int level, int mana, int xp, int armorValue, int spellsPerLevel) :
-	 base(type, name, hp, speed, dex, ac, magic, strength, symbol, ConsoleColor color, attack,
-	  mana, nameUsed, arcType, plHpPerLevBonus, level, mana, xp, armorValue, spellsPerLevel)
+        public static string nameUsed;
 
-	{
+        public PC(string name, ArchType profession, int maxHP, int maxAC, int speed, int dex, int ac, string magic, int strength, char symbol, ConsoleColor color, int attack,
+            int mana, string nameUsed, string arcType, int plHpPerLevBonus, int level, int armorValue, int spellsPerLevel)
 
-	}
 
-}
+        {
+            Name = name;
+            Profession = profession;
+            ArmorClass = maxAC;
+            MaxHP = maxHP;
+        }
 
-//    static void (NameMyCharacter)
-//{
-//Console WriteLine("Would you like to name your character (choice 1) or use default (choice 2)?")
-
-//if (Console readLine == "1" )
-//{
-//	  Console.WriteLine("What is your character's name");
-          
-//            nameUsed = Console.ReadLine();
-			
-//}
-//else if (Console readLine == "2" )
-//{
-//	nameUsed = Paladin.defaultName;
-//}
-//else
-//{
-//    Console WriteLine("That is not a valid choice please choose again");
-//    (NameMyCharacter);
-//}
-
-//}
+    }
 
 }
